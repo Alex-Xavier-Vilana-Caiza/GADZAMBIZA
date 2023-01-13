5d1a535174157c9944124e33e9eedb03ae124b66
Alex-Xavier-Vilana-Caiza <axc.vilana@yavirac.edu.ec>
2023-01-12 20:14:34 -0500

    Proyecto Finalizado

--- resources/views/fotos_galeria.blade.php
+++ resources/views/fotos_galeria.blade.php
@@ -5,7 +5,13 @@
 <div class="header-top">
     <div class="container d-flex justify-content-between">
         <div class="d-inline-flex ml-auto">
-            
+            <div class="headcont">
+                <i class="fas fa-2x fa-mobile-alt messenge"></i>
+                2886 280 / 0998757288
+            </div>
+            <div class="headcont">
+                <i class="fas fa-2x fa-envelope messenge"></i>
+                jldm605@gmail.com
             </div>
         </div>
     </div>
